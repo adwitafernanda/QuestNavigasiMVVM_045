375c6f597b2a27ab0359f1b6a5718b2b35680d20
adwitafernanda <adwitafernanda4@gmail.com>
2024-11-21 00:11:28 +0700

    menambahkan parameter

--- app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
@@ -1,10 +1,18 @@
 package com.example.simpleviewmodel.ui.viewmodel
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 enum class Halaman{
     Form,
     Data
 }
 @Composable
-fun Navigasi(){}
+fun Navigasi(
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController(),
+    modifier: Any
+){
+
+}
