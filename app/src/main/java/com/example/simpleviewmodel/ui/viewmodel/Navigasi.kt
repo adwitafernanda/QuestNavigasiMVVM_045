f674d3a3c427e8a2658b5a2ce3a805b535795f4d
adwitafernanda <adwitafernanda4@gmail.com>
2024-11-21 00:12:53 +0700

    menambahkan uiState

--- app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
@@ -1,6 +1,8 @@
 package com.example.simpleviewmodel.ui.viewmodel
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -14,5 +16,6 @@ fun Navigasi(
     navHost: NavHostController = rememberNavController(),
     modifier: Any
 ){
+    val uiState by viewModel.dataModel.collectAsState()
 
 }
